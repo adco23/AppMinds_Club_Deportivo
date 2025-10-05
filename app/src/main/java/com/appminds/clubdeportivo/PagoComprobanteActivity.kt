6a6f1f2b8a4a73c58c37939fd26cd4ef170d4c1f
Marcela <marcela.cordini@gmail.com>
2025-10-05 12:41:07 -0300

    buscacliente
    pero siguen los dos botones

--- app/src/main/java/com/appminds/clubdeportivo/PagoComprobanteActivity.kt
+++ app/src/main/java/com/appminds/clubdeportivo/PagoComprobanteActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -17,6 +19,14 @@ class PagoComprobanteActivity : AppCompatActivity() {
 
         val btnBack = findViewById<ImageButton>(R.id.btnGoToBack)
         btnBack.setOnClickListener { finish() }
+
+        val btnDescargar = findViewById<AppCompatButton>(R.id.btnDescargar)
+        btnDescargar.setOnClickListener {
+            Toast.makeText(this, "Descargando comprobante", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+
 
     }
 }
--- app/src/main/java/com/appminds/clubdeportivo/RegistrarPagoActivity.kt
+++ app/src/main/java/com/appminds/clubdeportivo/RegistrarPagoActivity.kt
@@ -16,17 +16,49 @@ import com.appminds.clubdeportivo.models.ClientDto
 import com.appminds.clubdeportivo.models.enums.ClientStatusEnum
 import com.appminds.clubdeportivo.models.enums.ClientTypeEnum
 
-
 class RegistrarPagoActivity : AppCompatActivity() {
+    private val clientsMock = listOf(
+        ClientDto("Lucía", "Gómez", "25678901", "Av. Belgrano 123, CABA", "1145678901", "lucia.gomez@mail.com", true, ClientTypeEnum.SOCIO, ClientStatusEnum.ACTIVO),
+        ClientDto("Martín", "Ríos", "30123456", "Calle Mitre 456, Mendoza", "2617894560", "martin.rios@mail.com", true, ClientTypeEnum.NO_SOCIO, ClientStatusEnum.INACTIVO),
+        ClientDto("Sofía", "Herrera", "28345678", "Av. Alem 789, Tucumán", "3816543210", "sofia.herrera@mail.com", true, ClientTypeEnum.SOCIO, ClientStatusEnum.ACTIVO),
+        ClientDto("Carlos", "Méndez", "26987654", "Calle San Martín 321, Rosario", "3419876543", "carlos.mendez@mail.com", true, ClientTypeEnum.NO_SOCIO, ClientStatusEnum.INACTIVO),
+        ClientDto("Valentina", "López", "29456789", "Av. Rivadavia 1590, Córdoba", "3511234567", "valentina.lopez@mail.com", true, ClientTypeEnum.SOCIO, ClientStatusEnum.ACTIVO)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_registrar_pago)
 
+        val searchInput = findViewById<EditText>(R.id.inputSearchClient)
+        val searchBtn = findViewById<AppCompatButton>(R.id.btnSearchClient)
+        val tlClientData = findViewById<TableLayout>(R.id.tlClientData)
+        val btnBack = findViewById<ImageButton>(R.id.btnBack)
         val btnPagarCuota = findViewById<Button>(R.id.btnPagarCuota)
         val btnPagarActividad = findViewById<Button>(R.id.btnPagarActividad)
 
-        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        val watcher = object  : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                searchBtn.isEnabled = searchInput.text.toString().trim().isNotEmpty()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+
+        searchInput.addTextChangedListener(watcher)
+        searchBtn.setOnClickListener {
+            tlClientData.isVisible = true
+            btnPagarCuota.isVisible = true
+            btnPagarActividad.isVisible = true
+        }
+
         btnBack.setOnClickListener { finish() }
+
+
+
+
+
+        //val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        //btnBack.setOnClickListener { finish() }
 
         btnPagarCuota.setOnClickListener {
             val intent = Intent(this, PagarCuotaActivity::class.java)
